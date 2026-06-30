6e4980a7c4bf1ccaac36f1f190f72f0189f3e706
JI YOON LEE <sundaeluv98@gmail.com>
2026-06-30 13:34:15 +0900

    [20260630] secondday-base code (viewbinding&layout)
    [20260630] secondday-base code (viewbinding&layout)

--- AndroidLab/app/src/main/java/com/example/androidlab/MainActivity.kt
+++ AndroidLab/app/src/main/java/com/example/androidlab/MainActivity.kt
@@ -8,11 +8,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.androidlab.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var button: Button
-    lateinit var editView: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,19 +18,16 @@ class MainActivity : AppCompatActivity() {
         //액티비티가 시스템 영역까지 차지하라
         enableEdgeToEdge()
 
-        //화면 출력 명령 : inflate(xml 에 명시된 뷰 객체 생성, 메모리에 올리는 작업) + 화면 출력
-        setContentView(R.layout.activity_main)
-        //필요한 뷰 객체 획득
-        button = findViewById(R.id.button)
-        editView = findViewById(R.id.editView)
-        //뷰에 이벤트 등록
-        button.setOnClickListener {
-            //유저 입력값 획득
-            val data = editView.text.toString()
+        // XXXBinding 클래스에 일은 시켜야 한다. 객체를 메모리에 올려달라. (inflate)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        //화면 출력 명령
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            val data = binding.editView.text.toString()
             // (옵션) 런타임 로그
             Log.d("MainActivity", data)
         }
-
 
         //시스템 영역에서 액티비티 컨텐츠를 보호해서 출력하기 위한 설정
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
--- AndroidLab/settings.gradle.kts
+++ AndroidLab/settings.gradle.kts
@@ -24,3 +24,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "AndroidLab"
 include(":app")
+include(":tripapp")

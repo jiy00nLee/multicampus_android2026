4bba09323fb609bf3980dc9e6ce3c0a6aed13435
JI YOON LEE <sundaeluv98@gmail.com>
2026-06-30 09:46:12 +0900

    [20260630] secondday-base code (refactoring)
    [20260630] secondday-base code (refactoring)

--- AndroidLab/app/build.gradle.kts
+++ AndroidLab/app/build.gradle.kts
@@ -31,6 +31,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    // ViewBinding== Generator. 선언하는 것만으로 layout xml 파일 하나당 XXXBinding 클래스 자동 generate 됨
+    viewBinding.enable= true
 }
 
 dependencies {

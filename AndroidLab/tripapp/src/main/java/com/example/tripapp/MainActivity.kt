fcb88a72ca786e916b2f21e4c2d977ef6a935014
JI YOON LEE <sundaeluv98@gmail.com>
2026-07-01 16:20:32 +0900

    [20260701] thirdday-lecture8 (actionbar/toolbar, menu)
    [20260701] thirdday-lecture8 (actionbar/toolbar, menu)

--- AndroidLab/tripapp/src/main/java/com/example/tripapp/MainActivity.kt
+++ AndroidLab/tripapp/src/main/java/com/example/tripapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.tripapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.addCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.tripapp.databinding.ActivityMainBinding
@@ -26,6 +29,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // 액티비티 윈도우의 액션바를 개발자가 지정한 툴바에 적용 필요.
+        setSupportActionBar(binding.toolbar)
 
         // 1번
         // object : 익명 클래스 선언 예약어.
@@ -60,10 +66,6 @@ class MainActivity : AppCompatActivity() {
             moveToActivity(MyInfoActivity::class.java)
         }
 
-        binding.btnMoveToSettingActivity.setOnClickListener {
-            moveToActivity(SettingActivity::class.java)
-        }
-
         // back button 이벤트 처리의 기본은 onKeyDown()으로 키 이벤트 처리가 기본
         // 허나 앱에서 백버튼 이벤트 처리의 비율이 높고 이벤트 처리 로직이여러개이경우가 있어,
         // api 33에서 onKeyDown()으로 백버튼 이벤트 처리가 deprecated 되었고 addCallback으로 별도 callback 등록 권장
@@ -78,8 +80,40 @@ class MainActivity : AppCompatActivity() {
                finish()
             }
         }
-
     }
+
+    // Menu View --------------------------------------------------------------------------------------------------------
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // 메뉴 출력
+        menuInflater.inflate(R.menu.menu_main, menu)
+        // SearchView가 적용된 MenuItem 객체 획득
+        val menuItem = menu?.findItem(R.id.menu_search)
+        val searchView = menuItem?.actionView as SearchView
+        // SearchView Event Listener 등록
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            // 검색을 위해 나온 키보드에 존재하는 검색 버튼 클릭 시
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
+                searchView.setQuery("", false)
+                searchView.isIconified = true // 아이콘으로 복귀
+                return true
+            }
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+        })
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    // 메뉴 이벤트 처리 자동 호출
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_setting -> moveToActivity(SettingActivity::class.java)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    
+    // Menu View --------------------------------------------------------------------------------------------------------
 
     fun moveToActivity(activityClass: Class<*>){
         val intent = Intent(this, activityClass)
